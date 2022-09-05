0606f5a5cb3ca9f450c2b3bb1d2ab625a61edf32
Christof Reichardt <chr78rm@onlinehome.de>
2022-09-05 17:10:58 +0200

    Beginning with JsonPublicKey implementation.

--- src/main/java/de/christofreichardt/json/webkey/JsonWebPublicKey.java
+++ src/main/java/de/christofreichardt/json/webkey/JsonWebPublicKey.java
@@ -1,16 +1,49 @@
 package de.christofreichardt.json.webkey;
 
+import java.security.PublicKey;
+import java.security.interfaces.ECPublicKey;
+import java.security.spec.AlgorithmParameterSpec;
+import java.security.spec.ECParameterSpec;
+
 final public class JsonWebPublicKey extends JsonWebKey {
 
+    public static Builder of(PublicKey publicKey) {
+        return new Builder(publicKey);
+    }
+
+    final PublicKey publicKey;
+    final AlgorithmParameterSpec algorithmParameterSpec;
+
     public JsonWebPublicKey(Builder builder) {
-        super(builder.kid, "");
+        super(builder.kid, builder.publicKey.getAlgorithm());
+        this.publicKey = builder.publicKey;
+        if (this.publicKey instanceof ECPublicKey ecPublicKey) {
+            this.algorithmParameterSpec = ecPublicKey.getParams();
+        } else {
+            this.algorithmParameterSpec = null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        String params = null;
+        if (this.algorithmParameterSpec instanceof ECParameterSpec ecParameterSpec) {
+            params = ecParameterSpec.toString();
+        }
+        return String.format("%s[kid=%s, keyType=%S, params=%s]", this.getClass().getSimpleName(), this.kid, this.keyType, params);
     }
 
     public static class Builder extends JsonWebKey.Builder<Builder> {
 
+        final PublicKey publicKey;
+
+        public Builder(PublicKey publicKey) {
+            this.publicKey = publicKey;
+        }
+
         @Override
         JsonWebPublicKey build() {
-            return null;
+            return new JsonWebPublicKey(this);
         }
     }
 }
---
+++ src/test/java/de/christofreichardt/json/webkey/JsonWebPublicKeyUnit.java
@@ -0,0 +1,96 @@
+package de.christofreichardt.json.webkey;
+
+import de.christofreichardt.diagnosis.AbstractTracer;
+import de.christofreichardt.diagnosis.Traceable;
+import de.christofreichardt.diagnosis.TracerFactory;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.ECPublicKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.AlgorithmParameterSpec;
+import java.security.spec.ECGenParameterSpec;
+import java.security.spec.ECParameterSpec;
+import java.security.spec.RSAKeyGenParameterSpec;
+import java.util.UUID;
+import org.assertj.core.api.WithAssertions;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class JsonWebPublicKeyUnit implements Traceable, WithAssertions {
+
+    @BeforeAll
+    void init() {
+        AbstractTracer tracer = getCurrentTracer();
+        tracer.entry("void", this, "init()");
+
+        try {
+        } finally {
+            tracer.wayout();
+        }
+    }
+
+    @Test
+    void withECPublicKey() throws NoSuchAlgorithmException, InvalidAlgorithmParameterException {
+        AbstractTracer tracer = getCurrentTracer();
+        tracer.entry("void", this, "withECPublicKey()");
+
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("EC");
+            ECGenParameterSpec ecGenParameterSpec = new ECGenParameterSpec("secp256r1");
+            keyPairGenerator.initialize(ecGenParameterSpec);
+            KeyPair keyPair = keyPairGenerator.generateKeyPair();
+            JsonWebPublicKey jsonWebPublicKey = JsonWebPublicKey.of(keyPair.getPublic())
+                    .build();
+            tracer.out().printfIndentln("jsonWebPublicKey = %s", jsonWebPublicKey);
+            assertThat(jsonWebPublicKey.keyType).isEqualTo("EC");
+            assertThat(jsonWebPublicKey.algorithmParameterSpec).isInstanceOf(ECParameterSpec.class);
+            assertThat(jsonWebPublicKey.publicKey).isInstanceOf(ECPublicKey.class);
+        } finally {
+            tracer.wayout();
+        }
+    }
+
+    @Test
+    void withKidAndRSAPublicKey() throws NoSuchAlgorithmException, InvalidAlgorithmParameterException {
+        AbstractTracer tracer = getCurrentTracer();
+        tracer.entry("void", this, "withKidAndRSAPublicKey()");
+
+        try {
+            String kid = UUID.randomUUID().toString();
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            AlgorithmParameterSpec algorithmParameterSpec = new RSAKeyGenParameterSpec(2048, RSAKeyGenParameterSpec.F4);
+            keyPairGenerator.initialize(algorithmParameterSpec);
+            KeyPair keyPair = keyPairGenerator.generateKeyPair();
+            JsonWebPublicKey jsonWebPublicKey = JsonWebPublicKey.of(keyPair.getPublic())
+                    .withKid(kid)
+                    .build();
+            assertThat(jsonWebPublicKey.keyType).isEqualTo("RSA");
+            assertThat(jsonWebPublicKey.algorithmParameterSpec).isNull();
+            assertThat(jsonWebPublicKey.publicKey).isInstanceOf(RSAPublicKey.class);
+            assertThat(jsonWebPublicKey.kid).isEqualTo(kid);
+        } finally {
+            tracer.wayout();
+        }
+    }
+
+    @AfterAll
+    void exit() {
+        AbstractTracer tracer = getCurrentTracer();
+        tracer.entry("void", this, "exit()");
+
+        try {
+        } finally {
+            tracer.wayout();
+        }
+    }
+
+    @Override
+    public AbstractTracer getCurrentTracer() {
+        return TracerFactory.getInstance().getCurrentPoolTracer();
+    }
+}
