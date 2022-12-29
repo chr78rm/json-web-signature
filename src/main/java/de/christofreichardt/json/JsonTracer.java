0541607209db0b293b88d9d114366c6b2d2e2612
Christof Reichardt <chr78rm@onlinehome.de>
2022-12-29 13:52:27 +0100

    Charset.forName() replaced by StandardCharsets.

--- src/main/java/de/christofreichardt/json/JsonTracer.java
+++ src/main/java/de/christofreichardt/json/JsonTracer.java
@@ -1,29 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package de.christofreichardt.json;
 
 import de.christofreichardt.diagnosis.AbstractTracer;
 import de.christofreichardt.diagnosis.Traceable;
+import jakarta.json.Json;
+import jakarta.json.JsonStructure;
+import jakarta.json.JsonWriter;
+import jakarta.json.JsonWriterFactory;
+import jakarta.json.stream.JsonGenerator;
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UncheckedIOException;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
-import jakarta.json.Json;
-import jakarta.json.JsonStructure;
-import jakarta.json.JsonWriter;
-import jakarta.json.JsonWriterFactory;
-import jakarta.json.stream.JsonGenerator;
 
 /**
- *
  * @author CReichardt
  */
 abstract public class JsonTracer implements Traceable {
@@ -44,14 +38,14 @@ abstract public class JsonTracer implements Traceable {
             try {
                 byte[] bytes;
                 ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-                try (JsonWriter jsonWriter = this.jsonWriterFactory.createWriter(byteArrayOutputStream, Charset.forName("UTF-8"));) {
+                try (JsonWriter jsonWriter = this.jsonWriterFactory.createWriter(byteArrayOutputStream, StandardCharsets.UTF_8)) {
                     jsonWriter.write(jsonStructure);
                 }
                 tracer.out().println();
                 bytes = byteArrayOutputStream.toByteArray();
                 try (ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
-                        InputStreamReader inputStreamReader = new InputStreamReader(byteArrayInputStream, Charset.forName("UTF-8"));
-                        BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
+                     InputStreamReader inputStreamReader = new InputStreamReader(byteArrayInputStream, StandardCharsets.UTF_8);
+                     BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
                     bufferedReader.lines().forEach(line -> tracer.out().printfIndentln(line));
                 }
                 tracer.out().println();
