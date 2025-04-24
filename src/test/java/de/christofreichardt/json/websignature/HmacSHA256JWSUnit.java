2ff857158292cf35d9357336ade9cc89f0e782a0
Christof Reichardt <chr78rm@onlinehome.de>
2025-04-24 17:55:48 +0200

    Test case added that validates an access token with the 'HS256' JSON web algorithm.

--- src/test/java/de/christofreichardt/json/websignature/HmacSHA256JWSUnit.java
+++ src/test/java/de/christofreichardt/json/websignature/HmacSHA256JWSUnit.java
@@ -4,6 +4,7 @@ import de.christofreichardt.diagnosis.AbstractTracer;
 import de.christofreichardt.diagnosis.Traceable;
 import de.christofreichardt.diagnosis.TracerFactory;
 import de.christofreichardt.json.JsonTracer;
+import de.christofreichardt.json.webkey.JsonWebSecretKey;
 import java.io.StringReader;
 import java.security.GeneralSecurityException;
 import java.security.InvalidKeyException;
@@ -145,7 +146,7 @@ public class HmacSHA256JWSUnit implements Traceable, WithAssertions {
      * Example taken from RFC 7515: JSON Web Signature (JWS).
      * @see <a href="https://www.rfc-editor.org/rfc/rfc7515.html#appendix-A.1">Example JWS Using HMAC SHA-256</a>
      * 
-     * @throws GeneralSecurityException 
+     * @throws GeneralSecurityException might be thrown during signing (not really)
      */
     @Test
     void hmacSha256WithStrings() throws GeneralSecurityException {
@@ -217,6 +218,69 @@ public class HmacSHA256JWSUnit implements Traceable, WithAssertions {
 
             final JWSSigner jwsSigner = new JWSSigner(joseHeader, payload);
             assertThatExceptionOfType(InvalidKeyException.class).isThrownBy(() -> jwsSigner.sign(keyPair.getPrivate()));
+        } finally {
+            tracer.wayout();
+        }
+    }
+
+    @Test
+    void oidcToken() throws GeneralSecurityException {
+        AbstractTracer tracer = getCurrentTracer();
+        tracer.entry("void", this, "oidcToken()");
+
+        try {
+            // This is extracted from a Keycloak realm JSON export
+            String strKeyMaterial = """
+                    {
+                      "id" : "db786a2e-67d9-4a83-b67c-53f83f50218c",
+                      "name" : "hmac-generated",
+                      "providerId" : "hmac-generated",
+                      "subComponents" : { },
+                      "config" : {
+                        "kid" : [ "cae8efe1-61c4-4210-8160-41b18fb50d77" ],
+                        "active" : [ "true" ],
+                        "secretSize" : [ "256" ],
+                        "secret" : [ "SsOhd4No_X8So710mA1iSVQhgTZsq04aRm4FmDPjyvpJRsYwS7SlA9hZjyKdcJWthdEtpQ3Ur1Anz7O8mUIWW-jI9qE-FT5z-HINqh2C4WJXh8uNDGLn8gJlMUXIXBahva5YcQtnzJQ-dPSo1FRYMxSzVWTnug4KOX06I1Ir4FQxf9citnBV3HK0M4EY5kPtXI5JtkSW6yBBnBsWyIjPHldpw6aF-u8KkgWuf-we2t7N-k3l6xgSnMGFTQvHTDDh9O_CfzHXjDv_FOZnoKfmXvMwq4_J8at6elynROesL8YR5ydhV3ClUwXkLS7xP_hFN_rh9JCoyvICP8h0Q4hJ3A" ],
+                        "priority" : [ "0" ],
+                        "enabled" : [ "true" ],
+                        "algorithm" : [ "HS256" ]
+                      }
+                    }""";
+            JsonObject keyMaterial;
+            try (StringReader stringReader = new StringReader(strKeyMaterial);
+                 JsonReader jsonReader = Json.createReader(stringReader)) {
+                keyMaterial = jsonReader.readObject();
+            }
+            String secret = keyMaterial.getJsonObject("config")
+                    .getJsonArray("secret")
+                    .getString(0);
+            byte[] secretBytes = JWSBase.decodeToBytes(secret);
+            SecretKeySpec secretKey = new SecretKeySpec(secretBytes, "HmacSHA256");
+            JsonWebSecretKey jsonWebSecretKey = JsonWebSecretKey.of(secretKey)
+                    .build();
+            String strOidcToken = """
+                    {
+                        "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJjYWU4ZWZlMS02MWM0LTQyMTAtODE2MC00MWIxOGZiNTBkNzcifQ.eyJleHAiOjE3NDU1MDE1NzAsImlhdCI6MTc0NTUwMTI3MCwianRpIjoiMGFhN2RjMWMtNTgwYy00NWRiLTlmOTYtMzA4M2Q1NmQzNzUxIiwiaXNzIjoiaHR0cHM6Ly9uZXh0LWtleWNsb2FrOjg0NDMvcmVhbG1zL3Rlc3QiLCJhdWQiOlsiaHMtMjU2LXRlc3QiLCJhY2NvdW50Il0sInN1YiI6IjBhMDJlMTFjLTc2NTMtNGZhOS1hZWE3LWMyNjA0ZDg1Yjk5NSIsInR5cCI6IkJlYXJlciIsImF6cCI6ImhzLTI1Ni10ZXN0Iiwic2lkIjoiOTlhNDg1YTAtMDI3MC00MGUwLThmMmQtMjdmNWQzZDExOTJkIiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyIvKiJdLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsiZGVmYXVsdC1yb2xlcy10ZXN0Iiwib2ZmbGluZV9hY2Nlc3MiLCJ1bWFfYXV0aG9yaXphdGlvbiIsImFwcC10ZXN0ZXIiLCJhcHAtdXNlciJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoiZW1haWwgcHJvZmlsZSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwibmFtZSI6IlRpbmEgVGVzdGVyIiwicHJlZmVycmVkX3VzZXJuYW1lIjoidGVzdGVyLTAiLCJnaXZlbl9uYW1lIjoiVGluYSIsImZhbWlseV9uYW1lIjoiVGVzdGVyIiwiZW1haWwiOiJ0aW5hLXRlc3RlckB4eXouYWJjIn0.W5TM0JinK6igWO-mBCzHqPEdIFLVoSG-skXri29sWGo",
+                        "expires_in": 300,
+                        "refresh_expires_in": 1800,
+                        "refresh_token": "eyJhbGciOiJIUzUxMiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICI5YTY3ZmRkZS03MzFmLTRhODUtYThmOS05NWMwMjAwNTg4YjIifQ.eyJleHAiOjE3NDU1MDMwNzAsImlhdCI6MTc0NTUwMTI3MCwianRpIjoiZGU3ZmQ1ZTMtNTk3NC00YTlhLTk4MDUtNjZiNGYzNjRmMTFiIiwiaXNzIjoiaHR0cHM6Ly9uZXh0LWtleWNsb2FrOjg0NDMvcmVhbG1zL3Rlc3QiLCJhdWQiOiJodHRwczovL25leHQta2V5Y2xvYWs6ODQ0My9yZWFsbXMvdGVzdCIsInN1YiI6IjBhMDJlMTFjLTc2NTMtNGZhOS1hZWE3LWMyNjA0ZDg1Yjk5NSIsInR5cCI6IlJlZnJlc2giLCJhenAiOiJocy0yNTYtdGVzdCIsInNpZCI6Ijk5YTQ4NWEwLTAyNzAtNDBlMC04ZjJkLTI3ZjVkM2QxMTkyZCIsInNjb3BlIjoid2ViLW9yaWdpbnMgcm9sZXMgYmFzaWMgZW1haWwgYWNyIHByb2ZpbGUifQ.DTQES5aPjcRguoZEML7247FvscyALYunDJif7sG6HId7vAo6flHyUJ4goC9LP9AjF3PTCu4NW0lRfTOQfric2g",
+                        "token_type": "Bearer",
+                        "not-before-policy": 0,
+                        "session_state": "99a485a0-0270-40e0-8f2d-27f5d3d1192d",
+                        "scope": "email profile"
+                    }""";
+            JsonObject oidcToken;
+            try (StringReader stringReader = new StringReader(strOidcToken);
+                 JsonReader jsonReader = Json.createReader(stringReader)) {
+                oidcToken = jsonReader.readObject();
+            }
+            String accessToken = oidcToken.getString("access_token");
+            JWSCompactSerialization compactSerialization = JWSCompactSerialization.of(accessToken);
+            boolean validated = JWS.createValidator()
+                    .compactSerialization(compactSerialization)
+                    .key(jsonWebSecretKey)
+                    .validate();
+            assertThat(validated).isTrue();
         } finally {
             tracer.wayout();
         }
